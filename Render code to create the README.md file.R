23eac87b9be7e25f316e6fbe77f81207f937c181
mdolan77 <mdolan@ncsu.edu>
2023-10-08 14:16:18 -0400

    Update Render code

--- Render code to create the README.md file.R
+++ Render code to create the README.md file.R
@@ -1,6 +1,6 @@
 # Code  to output the .Rmd file to a file called README.md 
 rmarkdown::render("C:/Users/dolan/Desktop/NC State Grad Program/ST558/Project 2/ST558_Project2/Project 2.Rmd",
                   output_format = "github_document",
-                  output_file = "C:/Users/dolan/Desktop/NC State Grad Program/ST558/Project 2/ST558_Project2/README.md"
-                  output_options = 
-                  html_preview=FALSE)
+                  output_file = "C:/Users/dolan/Desktop/NC State Grad Program/ST558/Project 2/ST558_Project2/README.md",
+                  output_options = list(
+                  html_preview=FALSE))
